1b7e974f50d669e2eba00318b46341dac4145ec3
zeynepceydanalbant <45692102+zeynepceydanalbant@users.noreply.github.com>
2020-10-25 20:10:45 +0300

    Add files via upload

---
+++ alt.php
@@ -0,0 +1,77 @@
+<!--border-top: 1px solid #ddd;-->
+
+<footer>
+        <div class="main-content">
+           <div class="left box">
+             <h2>About us</h2>
+             <div class="content">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia expedita voluptatem possimus repudiandae incidunt, dolorum iste ipsam, unde vero officia doloremque id, beatae aspernatur repellat autem cumque dolorem minima. Odit!</p>
+               <div class="social">
+                   <a href=""><span class="fab fa-facebook-f"></span></a>
+                   <a href=""><span class="fab fa-twitter"></span></a>
+                   <a href=""><span class="fab fa-instagram"></span></a>
+                   <a href=""><span class="fab fa-youtube"></span></a>
+               </div>
+             </div>
+           </div>
+           <div class="center box">
+            <h2>Address</h2>
+            <div class="content">
+                <div class="place">
+                    <span class="fas fa-map-marker-alt" style="color: white"></span>
+                    <span class="text">Birendranagar, Surkhet</span>
+                </div>
+                <div class="phone">
+                    <span class="fas fa-phone-alt" style="color: white"></span>
+                    <span class="text">+90-5535181908</span>
+                </div>
+                <div class="email">
+                    <span class="fas fa-envelope" style="color: white"></span>
+                    <span class="text">abc@example.com</span>
+                </div>
+            </div>
+           </div>
+           <div class="right box">
+               <h2>Contact Us</h2>
+               <div class="content">
+                   <form action="#">
+                       <div class="email">
+                           <div class="text">Email *</div>
+                           <input type="email" required>
+                       </div>
+                       <div class="msg">
+                           <div class="text">Message *</div>
+                           <textarea  cols="25" rows="2" required></textarea>
+                       </div>
+                       <div class="btn">
+                           <button type="submit">Send</button>
+                       </div>
+                   </form>
+               </div>
+           </div>
+
+        </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ ust.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+   <style>
+    .card-img-top {
+    transition: all .5s ease-in-out;
+    cursor: pointer;
+}
+    .card-img-top:hover {
+        transform: scale(1.5);
+        -webkit-transform: scale(1.5);
+    }
+    div.col{
+      padding: 15px 15px;
+    }
+    nav{
+      height:80px;
+    }
+    
+   /* .carousel-inner{
+      max-width:90%;
+      max-height:500px;
+      margin-left: auto;
+    margin-right: auto;
+    } */
+  
+.carousel-inner{
+  width:95%;
+  max-height: 732px !important;
+  margin: auto;
+}
+   
+   </style>
+  
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Ancaza Jewelry</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Anasayfa <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
